703ecbbab52d1daccdb6ec94047b4fac51dd2b18
sezimai <162865620+sezikss@users.noreply.github.com>
2024-08-02 15:17:13 +0600

    Time: 57 ms (62.48%), Space: 49944000 (66.17%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,17 +2,24 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-  const stack = [];
-  const characters = { ')': '(', '}': '{', ']': '['};
-for (const char of s) {
-      
-    if (!characters[char]){
-       stack.push(char); 
-    } 
-    else if (stack.pop() !== characters[char]){
+const isValid = (s) => {
+  let stack = [];
+  const map = new Map([
+    ['(', ')'],
+    ['{', '}'],
+    ['[', ']'],
+  ]);
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+      stack.push(s[i]);
+    } else {
+      const last = stack.pop();
+      if (s[i] !== map.get(last)) {
         return false;
-    } 
+      }
+    }
   }
+
   return stack.length === 0;
 };
