a3758e55c8b73c5032475d6051211aa5df8716df
sezimai <162865620+sezikss@users.noreply.github.com>
2024-08-09 20:38:42 +0600

    Time: 72 ms (65.61%), Space: 53436000 (9.37%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -1,9 +1,19 @@
-var reverse = function (num) {
-    const reversedNumber = parseInt(
-        Math.abs(num).toString().split('').reverse().join('')
-    );
-    if (reversedNumber > 2147483647) {
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function reverseInteger(x) {
+    let reversed = 0;
+    let sign = Math.sign(x); 
+    x = Math.abs(x); 
+    while (x > 0) {
+        let digit = x % 10;
+        reversed = reversed * 10 + digit; 
+        x = Math.floor(x / 10); 
+    }
+    reversed = reversed * sign;
+    if (reversed < -Math.pow(2, 31) || reversed > Math.pow(2, 31) - 1) { 
         return 0;
     }
-    return num < 0 ? -Math.abs(reversedNumber) : reversedNumber;
-};
+    return reversed;
+}
