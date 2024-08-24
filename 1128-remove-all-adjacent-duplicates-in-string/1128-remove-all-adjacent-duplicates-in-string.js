44d492a5ce284f5a1c71bf19bc8e23ce96f7668b
sezimai <162865620+sezikss@users.noreply.github.com>
2024-08-24 19:44:23 +0600

    Time: 62 ms (82.30%), Space: 57576000 (68.63%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(S) {
+    if(S.length < 2){
+        return S
+    }
+
+    let stack = [S.charAt(0)];
+    for(let i = 1; i< S.length; i++){
+        if(stack[stack.length - 1] == S.charAt(i)){
+            stack.pop();
+        }else{
+            stack.push(S.charAt(i));
+        }
+    }
+    return stack.join('');
+};
+
+console.log(removeDuplicates("abbaca"))
