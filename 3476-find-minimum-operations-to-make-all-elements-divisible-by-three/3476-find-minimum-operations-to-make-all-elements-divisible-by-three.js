b889f6ca08b76ab6aba0127b662655ad7804b205
sezimai <162865620+sezikss@users.noreply.github.com>
2024-09-13 20:38:07 +0600

    Time: 65 ms (25.48%), Space: 51204000 (14.40%) - LeetHub

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/3476-find-minimum-operations-to-make-all-elements-divisible-by-three.js
@@ -0,0 +1,10 @@
+var minimumOperations = function(nums) {
+    let operations = 0;
+    for (let num of nums){
+        let remainder = num % 3;
+        if (remainder !== 0){
+            operations += Math.min(remainder, 3 - remainder);
+        }
+    }
+    return operations;
+    }
